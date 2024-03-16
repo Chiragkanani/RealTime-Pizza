0f41dd61c06344181f5d50008a52ac50023be2f5
Chiragkanani <chiragkanani2003@gmail.com>
2024-03-16 12:16:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config()
 const path = require("path")
 const expresslayout = require("express-ejs-layouts")
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 const emitter = require('events');
 const session = require("express-session")
@@ -26,7 +26,7 @@ app.set('eventEmitter',eventEmitter);
 
 
 app.use(session({
-    secret: process.env.COOKIE_SECRET,
+    secret: process.env.COOKIE_SECRET || 'chirag kanani',
     resave: false,
     saveUninitialized: true,
     store: MongoDbStore.create({
